ca35dbc0f9321c9cd6c850945a32593e2bb0aca3
Das.Venkatesan <mobius2400@gmail.com>
2017-02-09 00:31:42 -0500

    Modified Search by Text Xml

--- app/src/main/java/com/venkatesan/das/cardmanager/MainActivity.java
+++ app/src/main/java/com/venkatesan/das/cardmanager/MainActivity.java
@@ -2,7 +2,10 @@ package com.venkatesan.das.cardmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void searchCardbyName(View v){
         EditText cardName = (EditText) findViewById(R.id.cardSearcher);
+        Button goSearch = (Button) findViewById(R.id.checkInventory2);
+
+        cardName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public void toSearchByText(View view)
+    {
+
     }
 }
